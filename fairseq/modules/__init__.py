a5fe9ebfc558d126be8159ef420a2f41e40d54bf
Dell <1701214310@pku.edu.cn>
2019-11-23 20:44:01 +0800

    separate Linear

--- fairseq/modules/__init__.py
+++ fairseq/modules/__init__.py
@@ -28,7 +28,7 @@ from .transformer_sentence_encoder import TransformerSentenceEncoder
 from .unfold import unfold1d
 from .transformer_layer import TransformerDecoderLayer, TransformerEncoderLayer
 from .vggblock import VGGBlock
-
+from .linear import Linear
 __all__ = [
     'AdaptiveInput',
     'AdaptiveSoftmax',
@@ -59,4 +59,5 @@ __all__ = [
     'TransformerEncoderLayer',
     'VGGBlock',
     'unfold1d',
+    'linear'
 ]
